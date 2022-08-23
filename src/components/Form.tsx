d653b3cea1b7c4636f2168106c7762685ad81c2c
Karol <kypacheco07@misena.edu.co>
2022-08-22 20:41:57 -0500

    first commit

---
+++ src/components/Form.tsx
@@ -0,0 +1,13 @@
+export const Form = () => {
+  return (
+    <>
+      <form method="onSubmit">
+        <input type="text" placeholder="names " />
+        <input type="text" placeholder="phone" />
+        <input type="email" placeholder="email" />
+        <input type="date" placeholder="date" />
+        <button>Submit</button>
+      </form>
+    </>
+  );
+};
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <>
+      <h5>Hello</h5>
+      <p> 🐞 </p>
+      <h6 style={{ fontWeight: "bold" }}>World</h6>
+    </>
+  );
+};
